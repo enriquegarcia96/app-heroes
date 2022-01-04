1865b32e28c397644fe89d2124cac14571578e19
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-01-03 18:39:28 -0600

    refactor: 💡 agregando el componente de agregar

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,10 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { MainPageComponent } from './main-page/main-page.component';
+import { PersonajesComponent } from './personajes/personajes.component';
+import { AgregarComponent } from './agregar/agregar.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, PersonajesComponent, AgregarComponent],
   exports: [MainPageComponent],
-  imports: [CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule],
 })
 export class DbzModule {}
