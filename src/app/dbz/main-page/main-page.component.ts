fc85ec25736303fdbbcf695a64867729f0aec239
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-01-03 18:36:25 -0600

    refactor: 💡 metodos para agregar un personaje

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,29 +1,28 @@
 import { Component } from '@angular/core';
-
-
-interface Personaje {
-  nombre: string;
-  poder: number;
-}
-
-
+import { Personaje } from '../interfaces/dbz.interface';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
+  personajes: Personaje[] = [
+    {
+      nombre: 'Kirby',
+      poder: 1000,
+    },
+    {
+      nombre: 'Link',
+      poder: 2000,
+    },
+  ];
 
   nuevo: Personaje = {
     nombre: 'Zelda',
-    poder: 10000
+    poder: 10555,
+  };
+
+  agregarNuevoPersonaje(argumento: Personaje){
+    this.personajes.push(argumento)
   }
-
-
-
-  agregar() {
-
-    console.log(this.nuevo);
-  }
-
 }
