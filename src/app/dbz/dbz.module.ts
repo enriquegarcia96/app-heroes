1bd4716f604cebe51e66792eceb02e30d480cc14
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-01-03 21:03:57 -0600

    refactor: 💡 injectando el servicio de DBZ

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -5,10 +5,12 @@ import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 import { PersonajesComponent } from './personajes/personajes.component';
 import { AgregarComponent } from './agregar/agregar.component';
+import { DbzService } from './services/dbz.service';
 
 @NgModule({
   declarations: [MainPageComponent, PersonajesComponent, AgregarComponent],
   exports: [MainPageComponent],
   imports: [CommonModule, FormsModule],
+  providers: [DbzService],
 })
 export class DbzModule {}
